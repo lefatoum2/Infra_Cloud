0334c2a6571d7c0e6f3af17ccf1b192576f637bf
lefatoum2 <73175706+lefatoum2@users.noreply.github.com>
2021-04-05 16:08:35 +0200

    Add files via upload

---
+++ api.py
@@ -0,0 +1,59 @@
+import psycopg2
+import uvicorn
+from fastapi import FastAPI
+
+
+class DataB:
+
+    @classmethod
+    def connexion(cls):
+        cls.host = 'database-1.cvuz5hbtumrs.us-east-2.rds.amazonaws.com'
+        cls.port = 5432
+        cls.user = 'postgres'
+        cls.password = '7165Jeanne?'
+        cls.database = 'exercises'
+        cls.con = psycopg2.connect(host=cls.host, port=5432, user=cls.user, password=cls.password,
+                                   database=cls.database)
+        cls.cur = cls.con.cursor()
+        cls.con.autocommit = True
+
+    @classmethod
+    def disconnect(cls):
+        cls.cur.close()
+        cls.con.close()
+
+    @classmethod
+    def facilities(cls):
+        DataB.connexion()
+        sql = 'select * from cd.facilities'
+        cls.cur.execute(sql)
+        res = cls.cur.fetchall()
+        return res
+
+    @classmethod
+    def costname(cls):
+        DataB.connexion()
+        sql = 'select name,membercost from cd.facilities'
+        cls.cur.execute(sql)
+        res = cls.cur.fetchall()
+        return res
+
+
+app = FastAPI(redoc_url=None)
+
+
+@app.get("/facilities")
+async def facilities():
+    data = DataB.facilities()
+    return data
+
+
+@app.get("/costname")
+async def costname():
+    data = DataB.costname()
+    return data
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='0.0.0.0',port=8081)
+
